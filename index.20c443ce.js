9e1c0137b42ebf5541c9cdd48445063698b05a88
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-12-07 10:29:05 +0000

    Updates

--- index.20c443ce.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".legal").innerHTML=`<span>&copy; ${(new Date).getFullYear()} by Trillo</span>`;
-//# sourceMappingURL=index.20c443ce.js.map
---
+++ index.7f76e6e6.js
@@ -0,0 +1,2 @@
+document.querySelector(".legal").innerHTML=`<span>&copy; ${(new Date).getFullYear()} by trillo</span>`;
+//# sourceMappingURL=index.7f76e6e6.js.map
